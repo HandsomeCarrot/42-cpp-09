6f2a4ea9b6eea5b3a78095c3c332cebca7687fdd
flex <bosssss.5000@gmail.com>
2026-02-19 19:33:31 +0100

    feat(ex00): add argument validation and error handling to main
    - maybe needs redesign

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,33 @@
 /*   By: vpoka <vpoka@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:46:06 by vpoka             #+#    #+#             */
-/*   Updated: 2026/02/19 14:50:20 by vpoka            ###   ########.fr       */
+/*   Updated: 2026/02/19 19:32:16 by vpoka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TODO
+#include <iostream>
+#include <exception>
+#include <stdexcept>
 
-int	main(void)
+/**
+ * @brief saves input file data */
+/*static void saveInputFileData(const char * file_path)
 {
+	(void)file_path;
+}*/
+
+int	main(int argc, char ** argv)
+{
+	(void)argv;
+	try
+	{
+		if (argc != 2)
+			throw std::runtime_error("expected exactly one file path argument.");
+	}
+	catch (const std::exception & e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
