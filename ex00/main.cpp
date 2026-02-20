9344b8f84aa4d6f305ba3b7ddb30c3cebf737087
flex <bosssss.5000@gmail.com>
2026-02-20 14:16:08 +0100

    refactor(ex00): simplify main.cpp and remove unused code

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,33 +6,30 @@
 /*   By: vpoka <vpoka@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:46:06 by vpoka             #+#    #+#             */
-/*   Updated: 2026/02/19 19:32:16 by vpoka            ###   ########.fr       */
+/*   Updated: 2026/02/20 14:15:32 by vpoka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
 #include <iostream>
-#include <exception>
-#include <stdexcept>
 
-/**
- * @brief saves input file data */
-/*static void saveInputFileData(const char * file_path)
-{
-	(void)file_path;
-}*/
 
 int	main(int argc, char ** argv)
 {
+	(void)argc;
 	(void)argv;
+
+	/*std::pair<std::string, std::string> pair;
 	try
 	{
 		if (argc != 2)
-			throw std::runtime_error("expected exactly one file path argument.");
+			throw std::runtime_error("expected exactly one argument. (input file path)");
 	}
 	catch (const std::exception & e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 		return (1);
-	}
+	}*/
+
 	return (0);
 }
