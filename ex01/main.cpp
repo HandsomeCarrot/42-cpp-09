bc4e38cc56c7d1b99c35725f2cbb91c01f864227
HandsomeCarrot <bosssss.5000@gmail.com>
2026-03-10 14:10:02 +0100

    wip(ex01): input parsing
    - only single digit numbers
    - no signs (+/-) in fornt of numbers
    - only operators supported (+, -, *, /)
    - everything has to be seperated by spaces (no characters can be right
      after each other)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,4 +1,40 @@
-int main(void)
+#include <exception>	//std::exception
+#include <iostream>		//std::cerr
+#include <stdexcept>	//std::runtime_error
+#include <string>		//std::string
+
+namespace
 {
+	std::string parseInput(int argc, char ** argv)
+	{
+		if (argc != 2)
+			throw std::runtime_error("invalid usage: expecetd exactly one argument");
+
+		std::string input = argv[1];
+
+		if (input.empty())
+			throw std::runtime_error("invalid input: empty argument");
+
+		// parse input
+
+		return (input);
+	}
+}
+
+int main(int argc, char ** argv)
+{
+	try
+	{
+		std::string input;
+
+		input = parseInput(argc, argv);
+
+		std::cout << input << std::endl;
+	}
+	catch (const std::exception & e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return (1);
+	}
 	return (0);
 }
